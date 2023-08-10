51498c949cb79ae969a4ecce99869874f552d8a2
orctom <orctom@gmail.com>
2023-08-10 18:37:43 +0800

    'params'

--- hao/es.py
+++ hao/es.py
@@ -92,30 +92,30 @@ class ES:
             raise ValueError(f'profile not configured: {self.profile}')
         self.client: Elasticsearch = invoker.invoke(connect, **self.conf)
 
-    def get_by_id(self, _id, index: str, **kwargs):
+    def get_by_id(self, _id, index: str, **params):
         assert _id is not None, '_id required'
         assert index is not None, 'index required'
 
         try:
-            return self.client.get(index=index, id=_id, **kwargs)
+            return self.client.get(index=index, id=_id, params=params)
         except NotFoundError:
             return None
 
-    def find_by_id(self, _id, index: str, **kwargs):
-        return self.get_by_id(_id, index, **kwargs)
+    def find_by_id(self, _id, index: str, **params):
+        return self.get_by_id(_id, index, **params)
 
-    def get_by_ids(self, _ids, index: str, **kwargs):
+    def get_by_ids(self, _ids, index: str, **params):
         assert _ids is not None and len(_ids) > 0, '_ids required and should not be empty'
         assert index is not None, 'index reequired'
 
         try:
-            result = self.client.mget(index=index, body={'ids': _ids}, **kwargs)
+            result = self.client.mget(index=index, body={'ids': _ids}, params=params)
             return result.get('docs') if result else None
         except NotFoundError:
             return None
 
-    def find_by_ids(self, _ids, index: str, **kwargs):
-        return self.get_by_ids(_ids, index, **kwargs)
+    def find_by_ids(self, _ids, index: str, **params):
+        return self.get_by_ids(_ids, index, **params)
 
     def count(self, query: dict, index: str, **params):
         assert query is not None and len(query) > 0, 'query required, and should not be empty'
@@ -139,14 +139,15 @@ class ES:
                highlight: Optional[dict] = None,
                highlight_fields: Optional[Union[dict, list]] = None,
                scroll: Optional[str] = None,
-               timeout=60):
+               timeout=60,
+               **params):
         assert query is not None and len(query) > 0, 'query required, and should not be empty'
         assert index is not None, 'index required'
         assert (highlight is None and highlight_fields is None) or (scroll is None), 'highlight not supported with scroll'
         if scroll is None or len(scroll) == 0:
-            yield from self._search(query, index, size, highlight, highlight_fields, timeout)
+            yield from self._search(query, index, size, highlight, highlight_fields, timeout, **params)
         else:
-            yield from self._search_scroll(query, index, size, scroll, timeout)
+            yield from self._search_scroll(query, index, size, scroll, timeout, **params)
 
     def _search(self,
                 query: dict,
@@ -154,7 +155,8 @@ class ES:
                 size=500,
                 highlight: Optional[dict] = None,
                 highlight_fields: Optional[Union[dict, list]] = None,
-                timeout=60):
+                timeout=60,
+                **params):
         pre_tags, post_tags = None, None
         if highlight:
             query['highlight'] = highlight
@@ -166,25 +168,14 @@ class ES:
             query['highlight'] = highlight
             pre_tags, post_tags = highlight.get('pre_tags'), highlight.get('post_tags')
 
-        data = self.client.search(
-            index=index,
-            size=size,
-            body=query,
-            request_timeout=timeout
-        )
+        data = self.client.search(index=index, size=size, body=query, request_timeout=timeout, params=params)
         hits = data['hits']['hits']
         do_escape = pre_tags and post_tags and len(pre_tags) == len(post_tags)
         for hit in hits:
             yield self._html_escape(hit, pre_tags, post_tags) if do_escape else hit
 
-    def _search_scroll(self, query: dict, index: str, size: int, scroll: str, timeout=60):
-        data = self.client.search(
-            index=index,
-            scroll=scroll,
-            size=size,
-            body=query,
-            request_timeout=timeout
-        )
+    def _search_scroll(self, query: dict, index: str, size: int, scroll: str, timeout=60, **params):
+        data = self.client.search(index=index, scroll=scroll, size=size, body=query, request_timeout=timeout, params=params)
         sid = data['_scroll_id']
         hits = data['hits']['hits']
         try:
@@ -224,27 +215,21 @@ class ES:
             item['highlight'] = {field: [convert(entry) for entry in entries] for field, entries in highlights.items()}
         return item
 
-    def aggs(self, query: dict, index: str, timeout=15):
+    def aggs(self, query: dict, index: str, timeout=15, **params):
         assert query is not None and len(query) > 0, 'query required, and should not be empty'
         assert index is not None, 'index required'
 
-        data = self.client.search(
-            index=index,
-            size=0,
-            body=query,
-            request_timeout=timeout
-        )
-
+        data = self.client.search( index=index, size=0, body=query, request_timeout=timeout, params=params)
         buckets = {k: v.get('buckets') for k, v in data.get('aggregations').items()}
         total = data['hits']['total']
         return buckets, total
 
-    def delete_by_id(self, _id, index: str, silent=True, timeout=30) -> bool:
+    def delete_by_id(self, _id, index: str, silent=True, timeout=30, **kwargs) -> bool:
         assert _id is not None, '_id required'
         assert index is not None, 'index required'
 
         try:
-            self.client.delete(index=index, id=_id, request_timeout=timeout)
+            self.client.delete(index=index, id=_id, request_timeout=timeout, params=kwargs)
             return True
         except NotFoundError as e:
             if silent:
@@ -252,12 +237,12 @@ class ES:
             else:
                 raise e
 
-    def delete_by_query(self, query, index: str, silent=True, timeout=30):
+    def delete_by_query(self, query, index: str, silent=True, timeout=30, **params):
         assert query is not None and len(query) > 0, 'query required, and should not be empty'
         assert index is not None, 'index required'
 
         try:
-            return self.client.delete_by_query(index=index, body=query, request_timeout=timeout)
+            return self.client.delete_by_query(index=index, body=query, request_timeout=timeout, params=params)
         except NotFoundError as e:
             if silent:
                 LOGGER.error(f"Failed to delete_by_query: {query}, index: {index}")
@@ -266,7 +251,7 @@ class ES:
             else:
                 raise e
 
-    def save(self, _id, doc, index: str, overwrite=True, silent: bool = True):
+    def save(self, _id, doc, index: str, overwrite=True, silent: bool = True, **params):
         assert _id is not None, '_id required'
         assert index is not None, 'index required'
 
@@ -276,7 +261,7 @@ class ES:
             index = index or self.index
             if not overwrite and self.is_exists(_id, index=index):
                 return
-            self.client.index(index, doc, id=_id)
+            self.client.index(index, doc, id=_id, params=params)
             return _id
         except Exception as e:
             if silent:
@@ -286,13 +271,13 @@ class ES:
             else:
                 raise e
 
-    def update(self, _id, doc, index: str):
+    def update(self, _id, doc, index: str, **params):
         assert _id is not None, '_id required'
         assert index is not None, 'index required'
 
         if doc is None:
             return
-        self.client.update(index, id=_id, body={'doc': doc})
+        self.client.update(index, id=_id, body={'doc': doc}, params=params)
 
     def is_exists(self, _id, index: str, source=False):
         assert _id is not None, '_id required'
