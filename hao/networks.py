623dd98cbaacae619eb62ed85c878b8a0b949b7c
orctom <orctom@gmail.com>
2024-09-02 11:58:41 +0800

    handle name not found error

--- hao/networks.py
+++ hao/networks.py
@@ -7,7 +7,10 @@ LOGGER = logs.get_logger(__name__)
 
 
 def is_open(host, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((host, port))
-    LOGGER.debug(f'{host}:{port} -> {result}')
-    return 0 == result
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((host, port))
+        LOGGER.debug(f'{host}:{port} -> {result}')
+        return 0 == result
+    except socket.gaierror:
+        return False
