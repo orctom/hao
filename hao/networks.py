5736b6a57d45666e4e8ec119d67f4adf35df20c4
orctom <orctom@gmail.com>
2024-10-12 18:14:42 +0800

    +timeout for is_open

--- hao/networks.py
+++ hao/networks.py
@@ -6,11 +6,12 @@ from . import logs
 LOGGER = logs.get_logger(__name__)
 
 
-def is_open(host, port):
+def is_open(host, port, timeout = 0.2):
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((host, port))
-        LOGGER.debug(f'{host}:{port} -> {result}')
-        return 0 == result
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(timeout)
+            result = sock.connect_ex((host, port))
+            LOGGER.debug(f'{host}:{port} -> {result}')
+            return 0 == result
     except socket.gaierror:
         return False
